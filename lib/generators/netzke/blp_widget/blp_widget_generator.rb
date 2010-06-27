154ec1deae6ea35eaf5c5f4fbf5d3ccde2bc725d
Kristian Mandrup <kmandrup@gmail.com>
2010-06-27 12:24:16 +0200

    blp widget improved

--- lib/generators/netzke/blp_widget/blp_widget_generator.rb
+++ lib/generators/netzke/blp_widget/blp_widget_generator.rb
@@ -4,8 +4,12 @@ module Netzke
   module Generators
     class BlpWidgetGenerator < Rails::Generators::NamedBase
       source_root File.expand_path('../templates', __FILE__)   
-  
-      argument :data_class, :type => :string, :required => true, :desc => 'Name of the data class to use in the grid'
+
+      class_option :center,  :type => :string, :desc => 'Center configuration. widget_class:data_class fx GridPanel:Boss'
+      class_option :south,  :type => :string, :desc => 'South configuration'
+      class_option :north,  :type => :string, :desc => 'North configuration '
+      class_option :east,   :type => :string, :desc => 'East configuration'
+      class_option :west,   :type => :string, :desc => 'West configuration'            
 
       def create_lib_netzke
         inside 'lib' do      
@@ -19,9 +23,49 @@ module Netzke
   
       protected
 
+      def regions_js
+        res = []
+        all_regions.each do |region|
+          res << %Q{
+          :#{region[:name]} => {
+            :widget_class_name => "#{region[:widget_class]}"#{data_class_js(region)}
+            :ext_config => {
+              :title => "#{title(region)}"
+            }
+          }}
+        end
+        res.join(",\n")
+      end
+
+      def title region
+        return region[:data_class].pluralize if region[:data_class]      
+        region[:widget_class]
+      end
+     
+      def data_class_js region
+        return %Q{,     
+            :data_class_name => "#{region[:data_class]}",} if region[:data_class]        
+        ""
+      end
+
+      def all_regions      
+        regions = []
+        [:center, :north, :south, :east, :west].each do |region|
+          regions << get_region(options[region], region) if options[region]
+        end                
+        regions
+      end
+
+      def get_region region, name
+        widget_class, data_class = region.split(':')
+        region = {:name => name, :widget_class => widget_class}
+        region.merge!(:data_class => data_class) if data_class
+        region
+      end  
+
       def class_name
         name.camelize
       end
-    end
+    end # class
   end
 end
